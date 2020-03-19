1a1e2395cc2011f5767eec352dc920b2a1100d11
VuDaiDuong-c0220h1 <61004399+VuDaiDuong-c0220h1@users.noreply.github.com>
2020-03-19 22:49:31 +0700

    Add files via upload

---
+++ src/year.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class year {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập năm cần kiểm tra: ");
+        int year = scanner.nextInt();
+        boolean isLeapYear = false;
+        boolean isDivisibleBy4 = year % 4 == 0;
+        if (isDivisibleBy4) {
+            boolean isDivisibleBy100 = year % 100 == 0;
+            if (isDivisibleBy100) {
+                boolean isDivisibleBy400 = year % 400 == 0;
+                if (isDivisibleBy400) {
+                    isLeapYear = true;
+                }
+            } else {
+                isLeapYear = true;
+            }
+        }
+        if (isLeapYear) {
+            System.out.printf("%d là năm nhuận", year);
+        } else {
+            System.out.printf("%d không là năm nhuận", year);
+        }
+    }
+}
